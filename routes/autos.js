ae7e540a0b07cc3570612b0f6bc38633c41378b2
00127417 <00127417@uca.edu.sv>
2018-12-01 14:22:09 -0600

    se creo el archivo autos en la carpeta routes y se completo con los metodos

---
+++ routes/autos.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    router = express.Router()
+    autoController = require('../controllers/autoController');
+
+//CREATE
+router.post('/', autoController.create);
+//READ
+router.get('/',autoController.getAll);
+router.get('/:id',autoController.getOne);
+//DELETE
+router.delete('/:id',autoController.delete);
+//UPDATE
+router.put('/:id',autoController.update);
